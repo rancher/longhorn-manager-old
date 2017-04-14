7dafcdc7de874e3ff7d4781a05122398e5e90451
Sheng Yang <sheng@yasker.org>
2017-04-13 21:01:25 -0700

    Add deployment script

---
+++ deploy/longhorn-deploy-node.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+set -e
+
+cleanup(){
+    name=$1
+    set +e
+    docker rm -vf ${name}
+    set -e
+}
+
+get_container_ip() {
+    container=$1
+    for i in `seq 1 5`
+    do
+        ip=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container`
+        if [ "$ip" != "" ]
+        then
+            break
+        fi
+        sleep 10
+    done
+
+    if [ "$ip" == "" ]
+    then
+        echo cannot find ip for $container
+        exit -1
+    fi
+    echo $ip
+}
+
+
+network=$1
+etcd_ip=$2
+
+if [ "$network" == "" -o "$etcd_ip" == "" ]; then
+        echo usage: $(basename $0) \<network_name\> \<etcd_server_ip\>
+        exit -1
+fi
+
+LONGHORN_BINARY_NAME="longhorn-binary"
+LONGHORN_BINARY_IMAGE="rancher/longhorn:b016f2d"
+
+LONGHORN_MANAGER_NAME="longhorn-manager"
+LONGHORN_MANAGER_IMAGE="rancher/longhorn-manager:31b613b"
+
+LONGHORN_DRIVER_NAME="longhorn-driver"
+LONGHORN_DRIVER_IMAGE="imikushin/storage-longhorn:8b1bb5c"
+
+# longhorn-binary first, provides binary to longhorn-manager
+cleanup ${LONGHORN_BINARY_NAME}
+
+docker run --name ${LONGHORN_BINARY_NAME} \
+        --network none \
+        ${LONGHORN_BINARY_IMAGE} \
+	/bin/bash
+echo ${LONGHORN_BINARY_NAME} is ready
+
+# now longhorn-manager
+cleanup ${LONGHORN_MANAGER_NAME}
+
+docker run -d \
+        --name ${LONGHORN_MANAGER_NAME} \
+        --network ${network} \
+        --privileged \
+        --uts host \
+        -v /dev:/host/dev \
+        -v /var/run:/var/run \
+        -v /var/lib/rancher/longhorn:/var/lib/rancher/longhorn \
+        --volumes-from ${LONGHORN_BINARY_NAME} \
+        ${LONGHORN_MANAGER_IMAGE} \
+        launch-manager -d \
+        --orchestrator docker \
+        --longhorn-image ${LONGHORN_BINARY_IMAGE} \
+        --etcd-servers http://${etcd_ip}:2379
+echo ${LONGHORN_MANAGER_NAME} is ready
+
+# finally longhorn-driver
+cleanup ${LONGHORN_DRIVER_NAME}
+
+docker run -d \
+        --name ${LONGHORN_DRIVER_NAME} \
+        --network none \
+        --privileged \
+        -v /run:/run \
+        -v /var/run:/var/run \
+        -v /dev:/host/dev \
+        -v /var/lib/rancher/volumes:/var/lib/rancher/volumes:shared \
+        ${LONGHORN_DRIVER_IMAGE}
+echo ${LONGHORN_DRIVER_NAME} is ready
+
+manager_ip=$(get_container_ip ${LONGHORN_MANAGER_NAME})
+
+echo Longhorn node is up at ${manager_ip}
---
+++ deploy/longhorn-deploy-ui.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+cleanup(){
+    name=$1
+    set +e
+    docker rm -vf ${name}
+    set -e
+}
+
+network=$1
+longhorn_manager_ip=$2
+
+if [ "$network" == "" -o "$longhorn_manager_ip" == "" ]; then
+        echo usage: $(basename $0) \<network\> \<longhorn_manager_ip\>
+        exit -1
+fi
+
+host_port=8080
+
+LONGHORN_UI_NAME="longhorn-ui"
+LONGHORN_UI_IMAGE="rancher/longhorn-ui:5528110"
+
+cleanup $LONGHORN_UI_NAME
+docker run -d \
+        --name ${LONGHORN_UI_NAME} \
+        --network ${network} \
+        -p ${host_port}:8000/tcp \
+        -e LONGHORN_MANAGER_IP=http://${longhorn_manager_ip}:9500 \
+        ${LONGHORN_UI_IMAGE}
+
+echo Longhorn UI is up at port ${host_port}
